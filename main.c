31e6c08fa133fd23effac2e81720d60e40112214
Björn <bjorn.agren@servicenow.com>
2022-06-07 09:51:20 +0200

    first

---
+++ main.c
@@ -0,0 +1,11 @@
+//
+//  main.c
+//  
+//
+//  Created by Björn Ågren on 2022-06-07.
+//
+
+#include "main.h"
+main() {
+    
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  
+//
+//  Created by Björn Ågren on 2022-06-07.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
